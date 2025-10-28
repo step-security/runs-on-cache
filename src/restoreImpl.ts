2b8a0c64697dc6701ccf4c5ed3f7105073b781f3
Raj-StepSecurity <rajkumar@stepsecurity.io>
2025-10-28 19:18:34 +0530

    chore: Cherry-picked changes from upstream

--- src/restoreImpl.ts
+++ src/restoreImpl.ts
@@ -69,6 +69,9 @@ export async function restoreImpl(
         }
 
         if (!cacheKey) {
+            // `cache-hit` is intentionally not set to `false` here to preserve existing behavior
+            // See https://github.com/actions/cache/issues/1466
+
             if (failOnCacheMiss) {
                 throw new Error(
                     `Failed to restore cache entry. Exiting as fail-on-cache-miss is set. Input key: ${primaryKey}`
@@ -80,7 +83,6 @@ export async function restoreImpl(
                     ...restoreKeys
                 ].join(", ")}`
             );
-
             return;
         }
 
